0cc06eb12fcb0bbaad3d6debe7579fd8736a298a
katlego-Mt <moimakatlego@outlook.com>
2021-09-10 11:17:48 +0200

    testing Post

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { UsersModule } from '../users/users.module';
+import { PassportModule } from '@nestjs/passport';
+import { LocalStrategy } from './local.strategy';
 
 @Module({
-  providers: [AuthService],
-  imports: [AuthService],
+  providers: [AuthService,LocalStrategy],
+  imports: [UsersModule,PassportModule],
 })
 
 export class AuthModule {}
